97d7e894963dcc818a92277979feaca16372d616
TheGhossst <116820865+TheGhossst@users.noreply.github.com>
2023-07-20 07:51:36 +0530

    Add files via upload

---
+++ srtf.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <limits.h>
+
+struct Process {
+    int pid;
+    int at; 
+    int bt; 
+    int wt; 
+    int tat; 
+    int rt; 
+};
+
+bool isAllProcessesCompleted(const struct Process* processes, int n) {
+    for (int i = 0; i < n; ++i) {
+        if (processes[i].rt > 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int findShortestRemainingTimeProcess(const struct Process* processes, int n, int currentTime) {
+    int shortestRemainingTime = INT_MAX;
+    int index = -1;
+
+    for (int i = 0; i < n; ++i) {
+        if (processes[i].at <= currentTime && processes[i].rt < shortestRemainingTime && processes[i].rt > 0) {
+            shortestRemainingTime = processes[i].rt;
+            index = i;
+        }
+    }
+    return index;
+}
+
+int main() {
+    int n; 
+    printf("Enter the number of processes: ");
+    scanf("%d", &n);
+
+    struct Process processes[n];
+    for (int i = 0; i < n; ++i) {
+        int pid, at, bt;
+        printf("Enter Process %d details:\n", i + 1);
+        printf("PID: ");
+        scanf("%d", &pid);
+        printf("Arrival Time: ");
+        scanf("%d", &at);
+        printf("Burst Time: ");
+        scanf("%d", &bt);
+        processes[i].pid = pid;
+        processes[i].at = at;
+        processes[i].bt = bt;
+        processes[i].wt = 0;
+        processes[i].tat = 0;
+        processes[i].rt = bt;
+    }
+
+    int currentTime = 0;
+    int completed = 0;
+    while (!isAllProcessesCompleted(processes, n)) {
+        int index = findShortestRemainingTimeProcess(processes, n, currentTime);
+
+        if (index == -1) {
+            currentTime++;
+            continue;
+        }
+        processes[index].rt--;
+        currentTime++;
+
+        if (processes[index].rt == 0) {
+            completed++;
+            processes[index].tat = currentTime - processes[index].at;
+            processes[index].wt = processes[index].tat - processes[index].bt;
+        }
+    }
+    double avgWaitingTime = 0.0;
+    double avgTurnaroundTime = 0.0;
+    for (int i = 0; i < n; ++i) {
+        avgWaitingTime += processes[i].wt;
+        avgTurnaroundTime += processes[i].tat;
+    }
+    avgWaitingTime /= n;
+    avgTurnaroundTime /= n;
+
+    printf("\nPID\tAT\tBT\tWT\tTAT\n");
+    for (int i = 0; i < n; ++i) {
+        printf("%d\t%d\t%d\t%d\t%d\n", processes[i].pid, processes[i].at, processes[i].bt, processes[i].wt, processes[i].tat);
+    }
+
+    printf("\nAverage Waiting Time: %lf\n", avgWaitingTime);
+    printf("Average Turnaround Time: %lf\n", avgTurnaroundTime);
+
+    return 0;
+}
